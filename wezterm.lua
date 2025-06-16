fab9b0364c251783c0d801cc9a19eb18c16e3485
homantix <lxhzzzzz@126.com>
2025-06-17 00:53:24 +0800

    update: change theme and add snippet for background

--- wezterm.lua
+++ wezterm.lua
@@ -7,7 +7,28 @@ config.leader = { key = "`", mods = "ALT", timeout_milliseconds = 1000 }
 
 config.keys = keys
 
-config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "Dracula (Gogh)"
+-- config.color_scheme = "Catppuccin Latte"
+config.color_scheme = "Sakura"
 config.tab_bar_at_bottom = true
 config.default_cursor_style = "SteadyBar"
+-- config.window_background_image = "/Users/homantix/Pictures/wallpaper/wallhaven-j3dgmy.jpg"
+--
+-- config.background = {
+-- 	{
+-- 		source = {
+-- 			-- File = "/Users/homantix/Pictures/wallpaper/wallhaven-6dr5rl.jpg",
+-- 			-- File = "/Users/homantix/Pictures/wallpaper/wallhaven-zpxzrj.png",
+-- 		},
+-- 		-- width = "100%",
+-- 		height = "100%",
+-- 		-- opacity = 0.9,
+-- 		hsb = {
+-- 			hue = 1.0,
+-- 			saturation = 1.0,
+-- 			brightness = 0.2,
+-- 		},
+-- 	},
+-- }
 return config
