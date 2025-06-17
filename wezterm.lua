3557696321bd2e3add23d734b800b1faa5869571
liuxuhao <lxhzzzzz@126.com>
2025-06-17 21:47:28 +0800

    update: mouse binding

--- wezterm.lua
+++ wezterm.lua
@@ -1,11 +1,13 @@
 local wezterm = require("wezterm")
 local keys = require("key_binding").Keys()
+local mouse_bindings = require("mouse_binding").MouseBindings()
 local config = wezterm.config_builder()
 config.font_size = 16
 
 config.leader = { key = "`", mods = "ALT", timeout_milliseconds = 1000 }
 
 config.keys = keys
+config.mouse_bindings = mouse_bindings
 
 -- config.color_scheme = "Catppuccin Mocha"
 -- config.color_scheme = "Dracula (Gogh)"
