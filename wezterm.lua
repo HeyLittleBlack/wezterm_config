70c731dd32f120b02632c448a690d7b27e3c0580
liuxuhao <lxhzzzzz@126.com>
2025-06-17 09:27:57 +0800

    update: add scroll bar

--- wezterm.lua
+++ wezterm.lua
@@ -10,4 +10,5 @@ config.keys = keys
 config.color_scheme = "Catppuccin Mocha"
 config.tab_bar_at_bottom = true
 config.default_cursor_style = "SteadyBar"
+config.enable_scroll_bar = true
 return config
